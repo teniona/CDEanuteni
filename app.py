2fc54550bc65a6668999b260cdd7ff3d45452d37
anugraha.das@vodafone.com <anugraha.das@vodafone.com>
2023-05-05 18:43:52 +0100

    adding flask app

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index_bootstrap.html')
+
+
+@app.route('/employee', methods=['POST'])
+def employee():
+    # extract employee data from request
+    data = request.json
+    first_name = data['first-name']
+    last_name = data['last-name']
+    age = data['age']
+    currently_employed = data['currently-employed']
+
+    # do something with the employee data (e.g. save to database)
+    # ...
+
+    # return a JSON response
+    response = {'employee_name': f'{first_name} {last_name}', 'employee_age': age, 'employee_currently_employed': currently_employed}
+    return jsonify(response)
+
+
+
+if __name__ == '__main__':
+    app.run()
+
--- server.py
+++ server.py
@@ -1,59 +1,73 @@
 import socket
 import threading
-import json
+from flask import Flask, request, jsonify
 
-def handle_client(client_socket, client_address):
-    print(f"Connected with {client_address}")
+app = Flask(__name__)
 
-    while True:
-        # receive data from client
-        try:
-            data = client_socket.recv(1024).decode()
-            if not data:
-                break
-        except Exception as e:
-            print(f"Error receiving data from client {client_address}: {e}")
-            break
+@app.route('/employee', methods=['POST'])
+def handle_employee_data():
+    # extract employee data from HTTP request
+    employee_data = request.get_json()
 
-        # parse JSON data received from client
-        try:
-            employee_data = json.loads(data)
-            print(f"Received data from {client_address}: {employee_data}")
-        except Exception as e:
-            print(f"Error parsing JSON data received from {client_address}: {e}")
-            continue
+    # process employee data
+    # here, can add code to store employee data in a database or file
+    # for now, just return the data as a JSON response
+    response_data = {
+        "status": "success",
+        "employee_name": f"{employee_data['first_name']} {employee_data['last_name']}",
+        "employee_age": employee_data['age'],
+        "employee_currently_employed": employee_data['currently_employed']
+    }
+    return jsonify(response_data)
 
-        # process employee data
-        # here, can add code to store employee data in a database or file
-        # for now, just print the data to console
-        print(f"Employee name: {employee_data['first_name']} {employee_data['last_name']}")
-        print(f"Employee age: {employee_data['age']}")
-        print(f"Employee currently employed: {employee_data['currently_employed']}")
+def handle_client_connection(client_socket):
+    try:
+        # receive data from the client
+        json_data = client_socket.recv(1024).decode()
 
-    client_socket.close()
-    print(f"Connection with {client_address} closed")
+        # send the data to the Flask app for processing
+        with app.test_request_context('/employee', method='POST', data=json_data):
+            response = app.full_dispatch_request()
 
+        # send the response back to the client
+        client_socket.send(response.get_data())
+    
+    except Exception as e:
+        print(f"Error handling client connection: {e}")
+        # send error response to the client
+        error_response = {
+            "status": "error",
+            "message": "An error occurred while processing your request"
+        }
+        client_socket.send(json.dumps(error_response).encode())
+    
+    finally:
+        # close the connection
+        client_socket.close()
 
-def start_server():
-    # create socket object
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # bind socket to a address
-    server_address = ('localhost', 8001)
-    server_socket.bind(server_address)
-
-    # listen for incoming connections (max backlog of 5)
-    server_socket.listen(5)
-    print("Server is listening on port 8000")
-
-    while True:
-        # wait for a connection
-        print("Waiting for a connection...")
-        client_socket, client_address = server_socket.accept()
-
-        # create a new thread to handle the client
-        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
-        client_thread.start()
 
 if __name__ == '__main__':
-    start_server()
+    # start the Flask app in a separate thread
+    flask_thread = threading.Thread(target=app.run, kwargs={'debug': False})
+    flask_thread.start()
+
+    # create a new socket object
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # get local machine name
+    server_address = ('localhost', 8001)
+
+    # bind the socket to a specific address and port
+    server_socket.bind(server_address)
+
+    # listen for incoming connections
+    server_socket.listen(1)
+    print(f"Server listening on {server_address}")
+
+    while True:
+        # accept a new connection
+        client_socket, client_address = server_socket.accept()
+
+        # handle the client connection in a separate thread
+        client_thread = threading.Thread(target=handle_client_connection, args=(client_socket,))
+        client_thread.start()
