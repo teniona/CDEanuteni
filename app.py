a42bd1c78087854b86381b26aa872fd619f310e2
anugraha.das@vodafone.com <anugraha.das@vodafone.com>
2023-05-16 12:35:01 +0100

    age bug fix

--- app.py
+++ app.py
@@ -10,19 +10,23 @@ def index():
 
 @app.route('/employee', methods=['POST'])
 def employee():
-    # extract employee data from request
-    data = request.json
-    first_name = data['first-name']
-    last_name = data['last-name']
-    age = data['age']
-    currently_employed = data['currently-employed']
+    try:
+        # extract employee data from request
+        data = request.json
+        first_name = data['first-name']
+        last_name = data['last-name']
+        age = data['age']
+        currently_employed = data['currently-employed']
 
-    # do something with the employee data (e.g. save to database)
-    # ...
+        # do something with the employee data (e.g. save to database)
+        # ...
 
-    # return a JSON response
-    response = {'employee_name': f'{first_name} {last_name}', 'employee_age': age, 'employee_currently_employed': currently_employed}
-    return jsonify(response)
+        # return a JSON response
+        response = {'employee_name': f'{first_name} {last_name}', 'employee_age': age, 'employee_currently_employed': currently_employed}
+        return jsonify(response)
+    except KeyError:
+        return jsonify({'error': 'Invalid request data. Please make sure all required fields are included.'}), 400
+
 
 
 
